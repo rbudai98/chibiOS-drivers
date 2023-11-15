a724db5ca11f957487fbecaac9aa5ffb79805d58
Robert Budai <Robert.Budai@analog.com>
2023-11-15 17:24:49 +0100

    sdpk1-adxl355: final version
    Signed-off-by: Robert Budai <Robert.Budai@analog.com>

--- RT-STM32F469I-EVAL-SDP-CK1Z-ADXL355/main.c
+++ RT-STM32F469I-EVAL-SDP-CK1Z-ADXL355/main.c
@@ -15,33 +15,16 @@
 #include "chprintf.h"
 #include <stdio.h>
 
-#include "chibios_gpio.h"
-#include "no_os_gpio.h"
-
 #include "chibios_spi.h"
 #include "no_os_spi.h"
 
 #include "adxl355.h"
 
 static THD_WORKING_AREA(waThreadBlinker, 256);
-
 static BaseSequentialStream* chp = (BaseSequentialStream*) &SD5;
-
-static struct chibios_gpio_init_param chibios_gpio_extra_ip_6 = {
-  .port = GPIOG,
-  .pad = 11U,
-  .mode = PAL_MODE_OUTPUT_OPENDRAIN,
-};
-
-static struct no_os_gpio_init_param chibios_GPIO_6 = {
-  .platform_ops = &chibios_gpio_ops,
-  .extra = &chibios_gpio_extra_ip_6,
-};
-
-static struct no_os_gpio_desc *gpio_desc_6;
-
 static struct adxl355_dev *adxl355_desc;
 
+// Chibi-OS
 static SPIConfig spicfg = {
   .circular         = false,
   .slave            = false,
@@ -52,19 +35,14 @@ static SPIConfig spicfg = {
   .cr2              = 0
 };
 
+// NO-Os
 static struct chibios_spi_init_param chibios_spi_extra_ip  = {
    .hspi=&SPID1,
    .spicfg=&spicfg,
 };
 
-
 static struct no_os_spi_init_param adxl355_spi_ip = {
-    .device_id = 1,
-    .max_speed_hz = 4000000,
-    .bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
-    .mode = NO_OS_SPI_MODE_0,
     .platform_ops = &chibios_spi_ops,
-    .chip_select = 15,
     .extra = &chibios_spi_extra_ip,
 };
 
@@ -73,13 +51,10 @@ static struct adxl355_init_param adxl355_ip = {
     .dev_type = ID_ADXL355,
 };
 
-
 static THD_FUNCTION(ThreadBlinker, arg) {
-
   (void)arg;
   chRegSetThreadName("blinker");
   while (true) {
-
     palToggleLine(LINE_LED_GREEN);
     chThdSleepMilliseconds(200);
   }
@@ -88,49 +63,6 @@ static THD_FUNCTION(ThreadBlinker, arg) {
 static struct adxl355_frac_repr x[32] = {0};
 static struct adxl355_frac_repr y[32] = {0};
 static struct adxl355_frac_repr z[32] = {0};
-
-
-static struct chibios_gpio_init_param chibios_gpio_extra_ip_1 = {
-  .port = GPIOB,
-  .pad = 3U,
-  .mode = PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST,
-};
-static struct no_os_gpio_init_param chibios_GPIO_1 = {
-  .platform_ops = &chibios_gpio_ops,
-  .extra = &chibios_gpio_extra_ip_1,
-};
-static struct chibios_gpio_init_param chibios_gpio_extra_ip_2 = {
-  .port = GPIOB,
-  .pad = 4U,
-  .mode = PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST,
-};
-static struct no_os_gpio_init_param chibios_GPIO_2 = {
-  .platform_ops = &chibios_gpio_ops,
-  .extra = &chibios_gpio_extra_ip_2,
-};
-static struct chibios_gpio_init_param chibios_gpio_extra_ip_3 = {
-  .port = GPIOA,
-  .pad = 7U,
-  .mode = PAL_MODE_ALTERNATE(5) | PAL_STM32_OSPEED_HIGHEST,
-};
-static struct no_os_gpio_init_param chibios_GPIO_3 = {
-  .platform_ops = &chibios_gpio_ops,
-  .extra = &chibios_gpio_extra_ip_3,
-};
-static struct chibios_gpio_init_param chibios_gpio_extra_ip_4 = {
-  .port = GPIOA,
-  .pad = 15U,
-  .mode = PAL_MODE_OUTPUT_PUSHPULL | PAL_STM32_OSPEED_HIGHEST,
-};
-static struct no_os_gpio_init_param chibios_GPIO_4 = {
-  .platform_ops = &chibios_gpio_ops,
-  .extra = &chibios_gpio_extra_ip_4,
-};
-
-static struct no_os_gpio_desc *gpio_desc_1;
-static struct no_os_gpio_desc *gpio_desc_2;
-static struct no_os_gpio_desc *gpio_desc_3;
-static struct no_os_gpio_desc *gpio_desc_4;
 
 /*
  * Application entry point.
@@ -148,11 +80,6 @@ int main(void) {
   chSysInit();
 
   chThdCreateStatic(waThreadBlinker, sizeof(waThreadBlinker), NORMALPRIO + 1, ThreadBlinker, NULL);
-
-  no_os_gpio_get(&gpio_desc_1, &chibios_GPIO_1);
-  no_os_gpio_get(&gpio_desc_2, &chibios_GPIO_2);
-  no_os_gpio_get(&gpio_desc_3, &chibios_GPIO_3);
-  no_os_gpio_get(&gpio_desc_4, &chibios_GPIO_4);
 
   int ret;
   adxl355_ip.comm_init.spi_init = adxl355_spi_ip;
@@ -172,7 +99,6 @@ int main(void) {
   sdStart(&SD5, NULL);
 
   while(1) {
-
        chprintf(chp, "Single read \n\r");
       ret = adxl355_get_xyz(adxl355_desc,&x[0], &y[0], &z[0]);
       if (ret)
@@ -180,8 +106,8 @@ int main(void) {
       chprintf(chp, " x=%d"".%09u", (int)x[0].integer, (abs)(x[0].fractional));
       chprintf(chp, " y=%d"".%09u", (int)y[0].integer, (abs)(y[0].fractional));
       chprintf(chp, " z=%d"".%09u \n\r", (int)z[0].integer,(abs)(z[0].fractional));
-
   }
+
 error:
   {
     while(true) {
